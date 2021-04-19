4b7989d887767ffab9409efe4cd4a4457db289bd
Arwa wanla <arwawanla@Arwas-MacBook-Pro-4.local>
2021-04-19 07:46:57 +0300

    Assignment files Commit

---
+++ Day008_ass2Checkorder/Day008_ass2Checkorder/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Day008_ass2Checkorder
+{
+    class Program
+    {
+        public static bool CheckOrder(string source)
+        {
+            //create stack of type string
+            string stack= " ";
+            //push first characeter, check the most recent number in the stack (top)
+            for (int i =0; i < source.Length; i++)
+            {
+                //push the first item to the stack
+                if (i == 0)
+                {
+                    stack = stack+source[i];
+                    //Console.WriteLine(stack);
+                    //Console.WriteLine(stack.Length);
+                    //Console.WriteLine(stack[0]);
+                    continue;
+                }
+                Console.WriteLine(stack[0]);
+                if (source[i] != stack[stack.Length-1])//if true
+                {
+                    stack += source[i];
+                    
+                    
+                }
+                else 
+                {
+                    stack = stack.Remove(stack.Length - 1);
+                }
+
+            }
+
+            Console.WriteLine(stack);
+
+            if (stack == " ")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
+        }
+        static void Main(string[] args)
+        {
+            string source = "11129923321";
+            Console.WriteLine(CheckOrder(source));
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ Day008_assi1_builder/Day008_assi1_builder/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Day008_assi1_builder
+{
+    class Program
+    {
+        class HTMLBuilder
+        {
+            private string value;
+
+            public HTMLBuilder()
+            {
+                this.value = "";
+            }
+
+
+            //startTag
+            public HTMLBuilder startTag(string val)
+            {
+                this.value += "<" + val + ">\n";
+                return this;
+            }
+            //contentTag
+            public HTMLBuilder contentTag(string val)
+            {
+                this.value += " " + val + " \n";
+                return this;
+            }
+            //meta data
+            public HTMLBuilder meta (string val)
+            {
+                this.value += "<meta " + val + ">\n";
+                return this;
+            }
+            //endTag
+            public HTMLBuilder endTag (string val)
+            {
+                this.value += "</" + val + ">\n";
+                return this;
+            }
+
+            //get to put every thing together
+            public string get()
+            {
+                return this.value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            HTMLBuilder builder = new HTMLBuilder();
+            
+
+            string result = builder.contentTag("<!DOCTYPE html>")
+                   .startTag("html")
+                        .startTag("title")
+                            .contentTag("Introduction to C#")
+                        .endTag("title")
+                        .meta("name=\"viewport\" content=\"width = device - width, initial - scale = 1\"")
+                        .startTag("body")
+                            .startTag("div class =\"Heder\"")
+                                .startTag("h1")
+                                    .contentTag("What is C# used for?")
+                                .endTag("h1")
+                            .endTag("div")
+                            .startTag("div class =\"container\"")
+                                .startTag("p")
+                                    .contentTag("C# is a general-purpose, modern and object-oriented programming language pronounced as “C sharp”. It was developed by Microsoft led by Anders Hejlsberg and his team within the .Net initiative and was approved by the European Computer Manufacturers Association (ECMA) and International Standards Organization (ISO). C# is among the languages for Common Language Infrastructure and the current version of C# is version 7.2. C# is a lot similar to Java syntactically and is easy for the users who have knowledge of C, C++ or Java.")
+                                .endTag("p")
+                            .endTag("div")
+                        .endTag("body")
+                   .endTag("html")
+                   .get();
+            Console.WriteLine(result);
+        }
+    }
+}
